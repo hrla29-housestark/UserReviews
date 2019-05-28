b7295c65100b0a0d4e109826eb6846faca2adc2e
Mark Hayford <mhayford29@gmail.com>
2019-05-28 12:06:26 -0700

    changed the component id from 'app' to 'reviews'

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 // import styles from "../dist/style.css";
 
-ReactDOM.render(<App id={12}/>, document.getElementById('app'))
+ReactDOM.render(<App id={12}/>, document.getElementById('reviews'))
